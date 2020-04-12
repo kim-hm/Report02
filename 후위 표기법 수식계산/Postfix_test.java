1ea77e8a9ac9b41e2fecb0ad6a63ad51e1c0f934
kim-hm <58060411+kim-hm@users.noreply.github.com>
2020-04-12 16:22:00 +0900

    후위 표기법 수식계란

---
+++ 후위 표기법 수식계산/Postfix_test.java
@@ -0,0 +1,10 @@
+public class Postfix_test {
+	public static void main(String[] args) {
+		optExp2 opt = new optExp2();
+		int result;
+		String exp = "35*62/-";
+		System.out.printf("\n후위표기식 : %s", exp);
+		result = opt.evalPostfix(exp);
+		System.out.printf("\n 연산결과 = %d \n", result);
+	}
+}
